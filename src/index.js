924874d273f307615accd86775494536a2854a2e
Adrian Villalobos <56200326+AdrianPaulV@users.noreply.github.com>
2024-07-09 17:11:52 -0400

    Revert "Merge pull request #4 from oslabs-beta/adrian-react-components"
    This reverts commit 2207d2527547d1442fb2751b9fa6efbb2ab651a6, reversing
    changes made to a28f8c7a5c036b52c174b002dce5bcfd64ba64c4.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -19,29 +19,8 @@ module.exports = {
         },
       },
       {
-        test: /\.module\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]__[hash:base64:5]', // More readable class names
-              },
-              sourceMap: true,
-            }
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /\.module\.s[ac]ss$/i,
+        test: /\.s?css/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/,

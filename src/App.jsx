0e0b3118ef30138c6a4e84208102b6e46ba9d180
Daniel Mahoney <daniel.mahoney55@gmail.com>
2024-07-09 11:33:31 -0400

    beggining of changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,32 @@ const App = () => {
   const [info, setInfo] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/getPods', { method: 'GET' })
+    fetch('http://localhost:3000/getPodInfo', { method: 'GET' })
       .then((res) => {
-        // console.log(res);
+        //console.log(res);
         res.json().then((json) => {
           console.log(json);
           // setInfo(prettyPrintJson.toHtml(json, { indent: 2 }));
           setInfo(JSON.stringify(json, null, 2));
+          console.log(json['ecs-test']);
+          console.log(
+            JSON.stringify({
+              name: json['ecs-test'].name,
+              resourceVersion: json['ecs-test'].resourceVersion,
+            })
+          );
+          fetch('http://localhost:3000/setWatch', {
+            method: 'POST',
+            body: JSON.stringify({
+              name: json['ecs-test'].name,
+              resourceVersion: json['ecs-test'].resourceVersion,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            //   'Access-Control-Allow-Methods': '*',
+            // },
+          }).then((res) => {});
         });
       })
       .catch((err) => {
